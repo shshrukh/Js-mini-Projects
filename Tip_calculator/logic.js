34763c4f339236518548e1d3d4d16823607a053a
shshrukh <shshrukh.arbaz@gmail.com>
2025-06-11 23:43:36 +0500

    complet the tip calculator

--- Tip_calculator/logic.js
+++ Tip_calculator/logic.js
@@ -2,7 +2,8 @@ const els = {};
 const state = {
     bill : 0,
     tip : 0,
-    people : 0
+    people : 0, 
+    Text : ''
 };
 
 function init(){
@@ -21,15 +22,48 @@ function casheDom (){
 
 
 function bindEvents(){
-    els.bill.addEventListener('keyup', handleAnswerTyping);
+    const inputArray = Array.from(els.inputs);
+    inputArray.forEach((iteams)=>{
+        iteams.addEventListener('keyup',handleAnswerTyping)
+    });
+
+    els.calcutateBtn.addEventListener('click', handleCalculation)
+
 
 }
 
 function handleAnswerTyping(e){
-    state.bill = e.target.value;
-    console.log('user typing', state.bill);
+    const storeIdValue = e.target.id;
+    if(storeIdValue === 'bill'){
+        state.bill = e.target.value;
+        console.log('user is typing', state.bill);
+        
+    }else if(storeIdValue === 'tip'){
+        state.tip = e.target.value;
+        console.log('user is typing', state.tip);
+    }else{
+        state.people = e.target.value;
+        console.log('user is typing', state.people);
+    }
     
 }
+function handleCalculation(){
+    const {bill,tip, people} = state;
+    const tipAmount = tip/100*bill;
+    const totalAmount = Number(bill)+Number(tipAmount);
+    console.log('total amount is ',totalAmount);
+    
+    const eachPersonPay = totalAmount/people;
+    state.Text = `your bill is ${bill}$ and tip is ${tipAmount}$ tota amount is ${totalAmount}$ and each person pay ${eachPersonPay}$`;
+    console.log(state);
+    render()
+    
+}
+
+function render(){
+    els.result.innerText = state.Text;
+}
+
 
 
 
